3c40cf17457ff452c40a7a4694e7546835f02bd2
Mathankumar R <rmathankumarcpm@gmail.com>
2023-04-13 15:10:01 +0530

    commit on 13/04 - 3 pm

--- agms/admin/includes/dbconnection.php
+++ agms/admin/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "agmsdb");
+$con=mysqli_connect("database-1.c7j0ofgsqkxi.us-east-1.rds.amazonaws.com", "admin", "examly2023", "artgmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
